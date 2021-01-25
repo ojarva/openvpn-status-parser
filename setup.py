f15dc97e36327808aace8c64f709e920c4c442ef
Olli Jarva <olli@jarva.fi>
2021-01-25 11:19:31 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='openvpn-status-parser',
-    version='0.0.2',
+    version='0.0.3',
     description='OpenVPN status parser',
     long_description=long_description,
     url='https://github.com/ojarva/openvpn-status-parser',
