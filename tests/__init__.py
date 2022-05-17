b24ae5664e366c088a6e716c122cb465a68c2554
Jared Oberhaus <joberhaus@google.com>
2022-05-17 19:46:36 +0800

    Make better tests for the valid test cases. Now it ensures that the resulting dictionaries match what is expected.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,16 +1,51 @@
 import datetime
 import glob
+import json
+import os
+import re
 import unittest
 
 import openvpn_status_parser
 import openvpn_status_parser.exceptions
 
 
+class Encoder(json.JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+
+
+def Decoder(d):
+    for f in ['connected_since', 'last_ref', 'timestamp']:
+        if f in d:
+            d[f] = datetime.datetime.strptime(d[f], "%Y-%m-%dT%H:%M:%S")
+    return d
+
+
 class TestValid(unittest.TestCase):
 
     def _test_file(self, filename):
         parsed = openvpn_status_parser.OpenVPNStatusParser(filename)
-        parsed.details
+
+        self._test_expectation(
+            filename, '.connected_clients', parsed.connected_clients)
+        self._test_expectation(
+            filename, '.routing_table', parsed.routing_table)
+        self._test_expectation(
+            filename, '.details', parsed.details)
+
+    def _test_expectation(self, filename, expected_extension, result):
+        expected_filename = re.sub('\.status$', expected_extension, filename)
+        print('checking against %s' % expected_filename)
+        if not os.path.exists(expected_filename):
+            print('Missing expected file %r:\n%s' % (
+                expected_filename, json.dumps(
+                    result, cls=Encoder, indent=4, sort_keys=True)))
+            raise Exception('Missing expected file %r' % expected_filename)
+        with open(expected_filename, 'r') as f:
+            expected = json.loads(f.read(), object_hook=Decoder)
+            self.assertDictEqual(expected, result)
 
 
 class TestBroken(unittest.TestCase):
