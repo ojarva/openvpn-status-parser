3f35b00e846493afed813c2c758d1798285b9439
Jared Oberhaus <joberhaus@google.com>
2022-05-17 17:42:59 +0800

    Test valid cases, they weren't being tested.
    Fix the the valid_1.status file, it had a space where it should have been a tab.

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,7 +6,15 @@ import openvpn_status_parser
 import openvpn_status_parser.exceptions
 
 
+class TestValid(unittest.TestCase):
+
+    def _test_file(self, filename):
+        parsed = openvpn_status_parser.OpenVPNStatusParser(filename)
+        parsed.details
+
+
 class TestBroken(unittest.TestCase):
+
     def _test_file(self, filename):
         parsed = openvpn_status_parser.OpenVPNStatusParser(filename)
 
@@ -20,10 +28,15 @@ def ch(filename):
     return lambda self: self._test_file(filename)  # pylint: disable=protected-access
 
 
+for fn in glob.glob("tests/testfiles/valid/*.status"):
+    print(f"Adding {fn}")
+    test_func_name = fn.replace(".", "_")
+    setattr(TestValid, f"test_{test_func_name}", ch(fn))
 for fn in glob.glob("tests/testfiles/broken/*.status"):
     print(f"Adding {fn}")
     test_func_name = fn.replace(".", "_")
     setattr(TestBroken, f"test_{test_func_name}", ch(fn))
+
 
 if __name__ == '__main__':
     unittest.main()
